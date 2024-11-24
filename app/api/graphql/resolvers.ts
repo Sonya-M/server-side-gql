0439ada3c8e79ee39f0bb6c0c178d36fa0f7beee
Sonja Musicki <sonja.musicki@gmail.com>
2024-11-24 22:43:27 +0100

    authentication

---
+++ app/api/graphql/resolvers.ts
@@ -0,0 +1,42 @@
+import { db } from '@/db/db'
+import { InsertIssues, SelectIssues, issues, users } from '@/db/schema'
+import { GQLContext } from '@/types'
+import { getUserFromToken, signin, signup } from '@/utils/auth'
+import { and, asc, desc, eq, or, sql } from 'drizzle-orm'
+import { GraphQLError } from 'graphql'
+
+export const resolvers = {
+  Query: {
+    me: (_parent, _args, ctx: GQLContext) => {
+      return ctx.user
+    },
+  },
+  Mutation: {
+    signin: async (_parent, args, ctx) => {
+      const { input } = args
+      const data = await signin(input)
+      if (!data || !data.user || !data.token) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: { code: 401 },
+        })
+      }
+      return {
+        ...data.user,
+        token: data.token,
+      }
+    },
+    createUser: async (_parent, args, ctx) => {
+      const { input } = args
+      const data = await signup(input)
+      if (!data || !data.user || !data.token) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: { code: 401 },
+        })
+      }
+      return {
+        ...data.user,
+        token: data.token,
+      }
+    },
+  },
+}
--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,9 +1,50 @@
-import { NextResponse } from 'next/server'
+import { startServerAndCreateNextHandler } from '@as-integrations/next'
+import { ApolloServer } from '@apollo/server'
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default'
+import { NextRequest, NextResponse } from 'next/server'
+import { schema } from './schema'
+import { resolvers } from './resolvers'
+import { getUserFromToken } from '@/utils/auth'
+
+// this will give you the apollo server playground
+let plugins = []
+if (process.env.NODE_ENV === 'production') {
+  plugins = [
+    ApolloServerPluginLandingPageProductionDefault({
+      embed: true,
+      graphRef: 'myGraph@prod',
+    }),
+  ]
+} else {
+  plugins = [ApolloServerPluginLandingPageLocalDefault({ embed: true })]
+}
+// end
+
+const server = new ApolloServer({
+  resolvers,
+  plugins,
+  typeDefs: schema,
+})
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  // this will run before your resolvers
+  context: async (req) => {
+    // attach user to ctx if already signed in
+    const user = await getUserFromToken(req.headers.get('authorization') ?? '')
+    return {
+      req,
+      user,
+    }
+  },
+})
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json({ data: {} })
+  return handler(request)
 }
 
 export async function POST(request: NextRequest) {
-  return NextResponse.json({ data: {} })
+  return handler(request)
 }

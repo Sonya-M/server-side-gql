944633ed34100b78ba0a97e83f5252ff49e49a10
Sonja Musicki <sonja.musicki@gmail.com>
2024-11-24 23:37:56 +0100

    crud

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -10,8 +10,50 @@ export const resolvers = {
     me: (_parent, _args, ctx: GQLContext) => {
       return ctx.user
     },
+    issues: async (parent, args, ctx) => {
+      if (!ctx.user) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: { code: 401 },
+        })
+      }
+      const { input } = args
+      const andFilters = [eq(issues.userId, ctx.user.id)]
+      if (input?.statuses) {
+        const statusFilters = input.statuses.map((status) =>
+          eq(issues.status, status)
+        )
+
+        andFilters.push(or(...statusFilters))
+      }
+
+      const data = await db.query.issues.findMany({
+        where: and(...andFilters),
+        orderBy: [
+          asc(sql`case ${issues.status}
+        when "backlog" then 1
+        when "inprogress" then 2
+        when "done" then 3
+      end`),
+          desc(issues.createdAt),
+        ],
+      })
+      return data
+    },
   },
   Mutation: {
+    createIssue: async (_parent, args, ctx: GQLContext) => {
+      if (!ctx.user) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: { code: 401 },
+        })
+      }
+      const { input } = args
+      const data = await db
+        .insert(issues)
+        .values({ userId: ctx.user.id, ...input })
+        .returning()
+      return data.at(0)
+    },
     signin: async (_parent, args, ctx) => {
       const { input } = args
       const data = await signin(input)
@@ -37,6 +79,63 @@ export const resolvers = {
         ...data.user,
         token: data.token,
       }
+    },
+
+    editIssue: async (_parent, args, ctx) => {
+      if (!ctx.user) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: { code: 401 },
+        })
+      }
+      const { input } = args
+      const { id, ...update } = input
+      const res = await db
+        .update(issues)
+        .set(update ?? {})
+        .where(and(eq(issues.userId, ctx.user.id), eq(issues.id, id)))
+        .returning()
+
+      return res.at(0)
+    },
+    deleteIssue: async (_, { id }, ctx) => {
+      if (!ctx.user)
+        throw new GraphQLError('UNAUTHORIZED', { extensions: { code: 401 } })
+
+      await db.delete(issues).where(eq(issues.id, id))
+      return id
+    },
+  },
+
+  IssueStatus: {
+    BACKLOG: 'backlog',
+    TODO: 'todo',
+    INPROGRESS: 'inprogress',
+    DONE: 'done',
+  },
+
+  Issue: {
+    user: (issue, args, ctx) => {
+      if (!ctx.user) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: { code: 401 },
+        })
+      }
+      return db.query.users.findFirst({
+        where: eq(users.id, issue.userId),
+      })
+    },
+  },
+
+  User: {
+    issues: (user, args, ctx) => {
+      if (!ctx.user) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: { code: 401 },
+        })
+      }
+      return db.query.issues.findMany({
+        where: eq(issues.userId, user.id),
+      })
     },
   },
 }
--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,10 +1,42 @@
 export const schema = `#graphql
 
+input EditIssueInput {
+  name: String
+  content: String
+  status: IssueStatus
+  id: ID!
+}
+
+type Issue {
+  createdAt: String!
+  id: ID!
+  name: String!
+  content: String!
+  userId: ID!
+  projectId: ID!
+  status: IssueStatus
+  user: User!
+}
+
+input CreateIssueInput {
+  name: String!
+  content:String!
+  status: IssueStatus
+}
+
+enum IssueStatus {
+  DONE 
+  TODO 
+  INPROGRESS
+  BACKLOG
+}
+
 type User {
  id: ID!
  email: String! 
  createdAt: String!
  token: String!
+ issues: [Issue!]!
 }
 
 input AuthInput {
@@ -12,12 +44,20 @@ input AuthInput {
   password:String!
 }
 
+input IssuesFilterInput {
+  statuses: [IssueStatus!]
+}
+
 type Query {
   me: User
+  issues(input: IssuesFilterInput): [Issue!]!
 }
 
 type Mutation {
   signin(input: AuthInput!): User
   createUser(input: AuthInput!): User 
+  createIssue(input: CreateIssueInput!): Issue
+  editIssue(input: EditIssueInput!): Issue!
+  deleteIssue(id: ID!): ID!
 }
 `
